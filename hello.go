8d89e096f278861f3cb460912f694f964bf2e1ca
Renã Souza <rena.souza@keyrus.com.br>
2018-09-26 12:08:25 -0300

    Scan and pointers

--- hello.go
+++ hello.go
@@ -6,11 +6,24 @@ import (
 )
 
 func main() {
-	var nome = "Renan"
-	idade := 24 //:= indica que eh uma declaracao de variavel
-	var versao float32 = 1.1
-	fmt.Println("Hello, sr.", nome, "Sua idade eh", idade)
+	nome := "Renan"
+	versao := 1.1
+	fmt.Println("Hello, sr.", nome)
 	fmt.Println("Este programa esta na versao", versao)
 
 	fmt.Println("O tipo da variavel versao eh", reflect.TypeOf(versao))
+
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir logs")
+	fmt.Println("0- Sair do Programa")
+
+	var comando int
+	fmt.Scanf("%d", &comando)
+	fmt.Scan(&comando)
+
+	fmt.Println("Digitado: ", comando)
+	fmt.Println("Address: ", &comando)
+
+	var ad = &comando
+	fmt.Println("Type of ad", reflect.TypeOf(ad))
 }
